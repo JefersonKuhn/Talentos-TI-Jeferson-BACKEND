cb8fa9c0b5da3ed28aebf0846cb1e995cd7867fa
JefersonKuhn <166512818+JefersonKuhn@users.noreply.github.com>
2024-07-01 11:45:25 -0300

    Add files via upload

---
+++ Talentos TI Jeferson BACKEND/script.js
@@ -0,0 +1,52 @@
+/*const nome = "Jeferson"
+let sobrenome = "Kuhn Dutra"
+var idade = 36
+console.log ("Meu nome é" , nome, sobrenome, "tenho", idade, "anos")
+
+console.log (` Meu nome é ${nome} ${sobrenome}, e tenho ${idade} anos.`)*/
+
+// Template string (tipo texto) não calcula
+//const nome = "Jeferson Kuhn Dutra"
+
+
+// Numbers (calcula valores)
+/*const mes = 7
+const temperatura = -3 
+const num1 = 25
+const num2 = 15
+
+console.log (num1 + num2)*/
+
+// Booleans
+/*let souEstudante = false
+let souProfessor = true*/
+
+// Exercício ( crie uma variavel com o nome de vocês, a idade e uma variavel que diga se vocês são estudantes ou não. Imprima a frase no console).
+/*const nome = "jeferson Kuhn"
+const idade = 36
+let souEstudante = true
+let souProfessor = false
+
+console.log (` Meu nome é ${nome}, e tenho ${idade} anos, você é estudante? ${souEstudante}.`)
+
+console.log (` Meu nome é ${nome}, e tenho ${idade} anos, você é Professor? ${souProfessor}.`)*/
+
+//string transformar em Numbers
+/*const num1 = "50"
+const num2 = "70"*/
+
+// transformação
+/*const newNum1 = Number(num1)
+const newNum2 = Number(num2)
+const soma = newNum1 + newNum2
+
+console.log(soma)*/
+
+// passando Numbers para strings
+//const num3 = 5
+//console.log(tipeof num3)
+//const num
+
+
+
+
