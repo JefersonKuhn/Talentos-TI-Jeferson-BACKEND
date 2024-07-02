8115bd2c7f15529d1760841500a7edd95c7486e7
JefersonKuhn <166512818+JefersonKuhn@users.noreply.github.com>
2024-07-02 11:43:43 -0300

    Add files via upload

---
+++ Talentos TI Jeferson BACKEND/script1.js
@@ -0,0 +1,136 @@
+
+// AULA= 01/07/24
+
+/*const nome = "Jeferson"
+let sobrenome = "Kuhn Dutra"
+var idade = 36
+console.log ("Meu nome é" , nome, sobrenome, "tenho", idade, "anos")
+
+console.log (` Meu nome é ${nome} ${sobrenome}, e tenho ${idade} anos.`)*/
+
+// Template string (tipo texto) não calcula
+//const nome = "Jeferson Kuhn Dutra"
+
+
+// Numbers (calcula valores)
+/*const mes = 7
+const temperatura = -3 
+const num1 = 25
+const num2 = 15
+
+console.log (num1 + num2)*/
+
+// Booleans
+/*let souEstudante = false
+let souProfessor = true*/
+
+// Exercício ( crie uma variavel com o nome de vocês, a idade e uma variavel que diga se vocês são estudantes ou não. Imprima a frase no console).
+/*const nome = "jeferson Kuhn"
+const idade = 36
+let souEstudante = true
+let souProfessor = false
+
+console.log (` Meu nome é ${nome}, e tenho ${idade} anos, você é estudante? ${souEstudante}.`)
+
+console.log (` Meu nome é ${nome}, e tenho ${idade} anos, você é Professor? ${souProfessor}.`)*/
+
+//string transformar em Numbers
+/*const num1 = "50"
+const num2 = "70"*/
+
+// transformação
+/*const newNum1 = Number(num1)
+const newNum2 = Number(num2)
+const soma = newNum1 + newNum2
+
+console.log(soma)*/
+
+// passando Numbers para strings
+//const num3 = 5
+//console.log(tipeof num3)
+//const num
+
+
+// AULA= 02/07/24
+
+//Set-ExecutionPolicy RemoteSigned 
+//npm install -g readline-sync
+
+/*const readlineSync = require('readline-sync');
+
+const num1 = Number(readlineSync.question('Insira um número'))
+const num2 = Number(readlineSync.question('Insira outro número'))
+
+console.log(num1 + num2)*/
+
+// Operadores Aritiméticos
+// Soma +
+// Subtração -
+// Multiplicação *
+// Divisão /
+
+// Comparadores
+
+//Igual (==)	
+//Não igual (!=)	
+//Estritamente igual (===)	
+//Estritamente não igual (!==)	
+//Maior que (>)	
+//Maior que ou igual (>=)	
+//Menor que (<)	
+//Menor que ou igual (<=)
+
+// Exercício 3
+
+// Faça 3 perguntas ao usuário, pedindo 3 notas.
+// Calculem a média destas notas.
+
+/*const num1 = Number(readlineSync.question('Insira uma nota'))
+const num2 = Number(readlineSync.question('Insira outra nota'))
+const num3 = Number(readlineSync.question('Insira mais uma nota'))
+
+const media = (num1 + num2 + num3)/3
+console.log(Sua média é ${media})*/
+
+/*const valor1 = 5
+const valor2 = 10
+
+console.log(valor1 === valor2)
+console.log(valor1 !== valor2)
+console.log(valor1 > valor2)
+console.log(valor1 >= valor2)
+console.log(valor1 < valor2)
+console.log(valor1 <= valor2)*/
+
+// Exercício 4
+
+// Peça para o usuário inserir dois números. compare estes números, e verifique se o primeiro número é 
+// maior, menor, igual, etc do que o segundo.
+
+/*const num1 = Number(readlineSync.question('Insira um número'))
+const num2 = Number(readlineSync.question('Insira outro número'))
+
+console.log(`${num1} é igual a ${num2}?`, num1 === num2)
+console.log(`${num1} é diferente de ${num2}?` , num1 !== num2)
+console.log(`${num1} é maior que ${num2}? `, num1 > num2)
+console.log(`${num1} é maior ou igual a ${num2}? `, num1 >= num2)
+console.log(`${num1} é menor que ${num2}? ` , num1 < num2)
+console.log(`${num1} é menor ou igual a ${num2}? ` , num1 <= num2)*/
+
+// Arrays
+
+
+
+
+// Exercícios 4
+
+//Crie um array compelo menos 5 raças de cachorro.
+//Peça para o usuário inserir um número de 0 a 4.
+//Imprima no console a raça correspondente à posição escolhida pelo usuário.
+
+
+
+
+
+
+
